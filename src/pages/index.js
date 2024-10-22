f770b322d0631c36b08520326f340f0744eb23ae
Phil Cunliffe <phillip.cunliffe@gmail.com>
2024-10-22 15:27:43 -0700

    Adding notes and highlights pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import NoteSection from '../components/note-section'
-import { GlobalStyle } from '../components/GlobalStyle'
 import { useStaticQuery } from 'gatsby'
+import HighlightSection from '../components/highlight-section'
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -18,7 +18,6 @@ const IndexPage = () => {
 
   return (
     <React.Fragment>
-      <GlobalStyle />
       { /* NavBar */}
       <Layout pageTitle="Home Page">
         <h1>Phil Cunliffe</h1>
@@ -27,6 +26,7 @@ const IndexPage = () => {
           <h2>Digital Garden</h2>
           <p className='description'>Learn more about ditigal gardens from <a href=''>Maggie Appleton</a></p>
           <NoteSection />
+          <HighlightSection />
         </div>
       </Layout>
       { /* Footer */}
