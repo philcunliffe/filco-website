ab52337dfa200c9022c708dee5e77b7f070ceb8e
Phillip Cunliffe <phillip.cunliffe@gmail.com>
2025-12-04 10:24:58 -0800

    removing gatsby code, slight style changes

--- src/components/stage-icon.js
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { stageToIcon } from "../utils/stageToIcon";
-
-const StageIcon = ({ stage, size }) => {
-  return <span>{stageToIcon(stage)}</span>;
-};
-
-export default StageIcon;
